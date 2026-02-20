f3e3d2d4701b5559a26027b8cede4a328166922b
LaMonaLisa34 <mona.vw@outlook.fr>
2026-02-20 16:13:44 +0100

    feat(backend): socket io and nginx

--- README.md
+++ README.md
@@ -139,3 +139,51 @@ pnpm -F web build
   <img src="./docs/Chatpage-Desktop.png" width="60%" alt="Chatpage Desktop">
   <img src="./docs/Chatpage-Mobile.png" width="14%" alt="Chatpage Mobile">
 </div>
+
+## Backend
+### Helmet
+Le middleware Helmet est utilisé pour renforcer la sécurité de l’API Express en ajoutant automatiquement plusieurs en-têtes HTTP de protection :
+- Protection contre certaines attaques XSS  
+- Prévention du clickjacking  
+- Sécurisation du type de contenu  
+- Gestion du referrer et des politiques de navigation  
+
+Pour test :   
+```bash
+curl -I http://localhost:3000/health
+```
+
+```bash
+HTTP/1.1 200 OK
+Content-Security-Policy: default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests
+Cross-Origin-Opener-Policy: same-origin
+Origin-Agent-Cluster: ?1
+Referrer-Policy: no-referrer
+Strict-Transport-Security: max-age=31536000; includeSubDomains
+X-Content-Type-Options: nosniff
+X-DNS-Prefetch-Control: off
+X-Download-Options: noopen
+X-Frame-Options: SAMEORIGIN
+X-Permitted-Cross-Domain-Policies: none
+X-XSS-Protection: 0
+Vary: Origin
+Content-Type: application/json; charset=utf-8
+Content-Length: 15
+ETag: W/"f-VaSQ4oDUiZblZNAEkkN+sX+q3Sg"
+Date: Fri, 20 Feb 2026 13:35:36 GMT
+Connection: keep-alive
+Keep-Alive: timeout=5
+```
+
+### Rate Limit
+Permet de limite le nombre de request (evite spam et attaques DoS): 
+``` js
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requêtes
+});
+```
+Pour tester : 
+``` bash
+for i in {1..200}; do curl http://localhost:3000/health; done
+```
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,117 @@
-import express  from "express";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server, Socket } from "socket.io";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+// Security middleware
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
-app.get("/health", (_req, res) => {
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://socket-io-front.vercel.app"],
+  })
+);
+
+const server = http.createServer(app);
+
+type ServerToClientEvents = {
+  "users:count": (count: number) => void;
+  "room:message": (msg: ChatMessage) => void;
+  "chat:message": (msg: ChatMessage) => void;
+};
+
+type ClientToServerEvents = {
+  "room:join": (roomId: string) => void;
+  "room:leave": (roomId: string) => void;
+  "room:message": (data: { room: string; message: string }) => void;
+  "chat:message": (msg: string) => void;
+};
+
+const io = new Server<ClientToServerEvents, ServerToClientEvents>(server, {
+  cors: {
+    origin: ["http://localhost:5173", "https://socket-io-front.vercel.app"],
+  },
+});
+
+
+type ChatMessage = {
+  text: string;
+  time: string;
+  date: string;
+  timestamp: Date;
+};
+
+const formatMessage = (text: string): ChatMessage => {
+  const now = new Date();
+
+  return {
+    text,
+    time: now.toLocaleTimeString("fr-FR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    }),
+    date: now.toLocaleDateString("fr-FR"),
+    timestamp: now,
+  };
+};
+
+
+app.get("/health", (_, res) => {
   res.json({ status: "ok" });
 });
 
-app.listen(PORT, () => {
-  console.log("API running on port", PORT);
+
+
+io.on("connection", (socket: Socket<ClientToServerEvents, ServerToClientEvents>) => {
+  console.log("Connected:", socket.id);
+
+  socket.on("room:join", (roomId) => {
+    socket.join(roomId);
+
+    io.to(roomId).emit(
+      "room:message",
+      formatMessage(`Un utilisateur a rejoint la room`)
+    );
+  });
+
+  socket.on("room:leave", (roomId) => {
+    socket.leave(roomId);
+
+    io.to(roomId).emit(
+      "room:message",
+      formatMessage(`Un utilisateur a quitté la room`)
+    );
+  });
+
+
+  socket.on("room:message", ({ room, message }) => {
+    io.to(room).emit("room:message", formatMessage(message));
+  });
+
+  // socket.on("chat:message", (msg) => {
+  //   io.emit("chat:message", formatMessage(msg));
+  // });
+
+  // socket.on("disconnect", () => {
+  //   connectedUsers--;
+  //   io.emit("users:count", connectedUsers);
+  // });
+});
+
+server.listen(3000, () => {
+  console.log("API running on port 3000");
 });
